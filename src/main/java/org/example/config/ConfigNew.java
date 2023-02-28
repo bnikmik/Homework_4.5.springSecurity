af52bd8803f685d7a8f2bcdba4ba460cd791513a
Nikita Bolshakov <bnikmik@gmail.com>
2023-02-28 15:25:13 +0300

    done

--- src/main/java/org/example/config/ConfigNew.java
+++ src/main/java/org/example/config/ConfigNew.java
@@ -1,12 +1,16 @@
 package org.example.config;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
@@ -21,4 +25,17 @@ public class ConfigNew {
         return viewResolver;
     }
 
+    @Bean
+    public DataSource getDataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost/skypro");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("123");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
+    }
 }
--- src/main/java/org/example/config/SecurityConfigNew.java
+++ src/main/java/org/example/config/SecurityConfigNew.java
@@ -1,21 +1,37 @@
 package org.example.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfigNew extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfigNew(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("Ivan").password("qwerty").roles("Employee"))
-                .withUser(userBuilder.username("Petr").password("asdfgh").roles("IT"))
-                .withUser(userBuilder.username("Olga").password("zxcvbn").roles("Security"))
-                .withUser(userBuilder.username("Ekaterina").password("123456").roles("Director"));
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/request_access").hasAnyRole("Employee", "IT", "Security", "Director")
+                .antMatchers("/employee_request").hasAnyRole("Employee", "IT", "Security", "Director")
+                .antMatchers("/it_request").hasAnyRole("IT", "Security", "Director")
+                .antMatchers("/security_request").hasAnyRole("Security", "Director")
+                .antMatchers("/director_request").hasAnyRole("Director")
+                .and().formLogin().permitAll();
     }
 }
